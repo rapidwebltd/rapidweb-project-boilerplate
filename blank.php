d0067022179307a812477db3de5120944f1843d7
Ollie <ojreardon@gmail.com>
2017-01-13 10:13:34 +0000

    Added container class w/ placeholder text

--- blank.php
+++ blank.php
@@ -1,13 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<?php require($_SERVER['DOCUMENT_ROOT'].'/layout_inc/head-script.php'); ?>
+		<?php require_once($_SERVER['DOCUMENT_ROOT'].'/layout_inc/head-script.php'); ?>
 		<title></title>
 	</head>
 	<body>
-		<?php require($_SERVER['DOCUMENT_ROOT'].'/layout_inc/header.php'); ?>
+		<?php require_once($_SERVER['DOCUMENT_ROOT'].'/layout_inc/header.php'); ?>
+		<div class="container">
+			<h1>Page Title</h1>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+		</div>
 
-
-		<?php require($_SERVER['DOCUMENT_ROOT'].'/layout_inc/footer.php'); ?>		
+		<?php require_once($_SERVER['DOCUMENT_ROOT'].'/layout_inc/footer.php'); ?>		
 	</body>
 </html>
